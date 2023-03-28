931bcaac850c3188c39272baec86c0a59a6e024e
Murasya <s2120644@s.tsukuba.ac.jp>
2023-03-29 00:14:34 +0900

    ビルド時にメッセージをつけれるように変更

--- build.sh
+++ build.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 
+# TODO: タグの名前を変更すること
+MESSAGE=`git log ios\#10android\#12.. --oneline`
+echo "$MESSAGE"
+
 echo "ios build..."
 flutter build ipa --export-options-plist="ios/ExportOptions.plist"
 echo "ios uploading..."
-dg deploy build/ios/ipa/routine_app.ipa
+dg deploy build/ios/ipa/routine_app.ipa --message $MESSAGE
 
 echo "android build..."
 flutter build appbundle
 echo "android uploading..."
-dg deploy build/app/outputs/bundle/release/app-release.aab
+dg deploy build/app/outputs/bundle/release/app-release.aab --message $MESSAGE
