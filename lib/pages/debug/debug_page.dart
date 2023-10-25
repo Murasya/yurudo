6a2ebf6496abfe8c78bb93d1b64fa9746b60749e
Murasya <s2120644@s.tsukuba.ac.jp>
2023-10-25 22:44:00 +0900

    デバッグメニューに前日のタスク作成を追加

--- lib/pages/debug/debug_page.dart
+++ lib/pages/debug/debug_page.dart
@@ -1,12 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:routine_app/design/app_color.dart';
 import 'package:routine_app/services/notification_service.dart';
+import 'package:routine_app/utils/contextEx.dart';
+import 'package:routine_app/viewModel/todo_provider.dart';
 
-class DebugPage extends StatelessWidget {
+class DebugPage extends ConsumerWidget {
   const DebugPage({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('デバッグメニュー'),
@@ -24,9 +27,27 @@ class DebugPage extends StatelessWidget {
             title: const Text('1分後に通知を送る'),
             onTap: () {
               NotificationService().testNotification();
-              ScaffoldMessenger.of(context).showSnackBar(
+              context.showSnackBar(
                 const SnackBar(
                   content: Text('通知をセットしました'),
+                ),
+              );
+            },
+          ),
+          ListTile(
+            title: const Text('前日のタスクを作る'),
+            onTap: () {
+              ref.read(todoProvider.notifier).create(
+                    name: '前日のタスク',
+                    span: 1,
+                    firstDay: DateTime.now().add(const Duration(days: -1)),
+                    remind: true,
+                    categoryId: 0,
+                    time: 5,
+                  );
+              context.showSnackBar(
+                const SnackBar(
+                  content: Text('前日のタスクを作りました'),
                 ),
               );
             },
