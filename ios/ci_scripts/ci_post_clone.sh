2eb9ed37696369ce2b74675d2a81b59fb1e0aa59
Murasya <s2120644@s.tsukuba.ac.jp>
2024-09-14 12:55:09 +0900

    ios CI/CD

---
+++ ios/ci_scripts/ci_post_clone.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+
+# Fail this script if any subcommand fails.
+set -e
+
+# The default execution directory of this script is the ci_scripts directory.
+cd $CI_PRIMARY_REPOSITORY_PATH # change working directory to the root of your cloned repo.
+
+# Install Flutter using git.
+git clone https://github.com/flutter/flutter.git --depth 1 -b stable $HOME/flutter
+export PATH="$PATH:$HOME/flutter/bin"
+
+# Install Flutter artifacts for iOS (--ios), or macOS (--macos) platforms.
+flutter precache --ios
+
+# Install Flutter dependencies.
+flutter pub get
+
+# Install CocoaPods using Homebrew.
+HOMEBREW_NO_AUTO_UPDATE=1 # disable homebrew's automatic updates.
+brew install cocoapods
+
+# Install CocoaPods dependencies.
+cd ios && pod install # run `pod install` in the `ios` directory.
+
+# make secret files
+echo ${env_file} > assets/env/dev.json
+echo ${firebase_app_id_file} > ios/firebase_app_id_file.json
+echo ${GoogleService_Info} > ios/Runner/GoogleService-Info.plist
+
+flutter build ios --config-only --flavor dev
+
+exit 0
